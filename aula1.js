b0cc4ab2985a7cf50848a819d7b5ee8da51ed332
Arthur Romanha <126591597+ArthurRomanha@users.noreply.github.com>
2025-05-08 09:56:35 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,44 @@
+//                      Desafio 1
+// class Pessoa{
+//     constructor(nome, altura, apelido, idade){
+//         this.nome = nome
+//         this.altura = altura
+//         this.apelido = apelido
+//         this.idade = idade
+//     }
+//     apresentar(apresentacao){
+//         console.log(apresentacao);
+//     }
+// }
+// let carol = new Pessoa("Carol", 1.2, "Of T Bells", 90)
+// console.log(carol);
+
+// carol.apresentar(`Oii, eu sou a ${carol.nome} e tenho ${carol.idade} anos`);
+
+//                      Desafio 2
+class Produto{
+    constructor(nome, preco, quantidade){
+        this.nome = nome
+        this.preco = preco
+        this.quantidade = quantidade
+    }
+    mostraDados(){
+        console.log(`O produto: ${this.nome} possui ${this.quantidade} unidades`);
+    }
+    vender(qtd){
+        this.quantidade-=qtd;
+        console.log(`Venda de ${qtd} unidades de ${this.nome} realizada`);
+        this.mostraDados()
+
+    }
+    repor(qtd){
+        this.quantidade+=qtd;
+        console.log(`Reposição de ${qtd} unidades de ${this.nome} realizada`);
+        this.mostraDados()
+    }
+}
+let maca = new Produto("Maçã", 4.5, 10);
+maca.mostraDados()
+
+maca.vender(10)
+maca.repor(20)
